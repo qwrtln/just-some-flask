380e0906c233657a35d71fe7c5fc49fa74b52112
Andrzej Wiącek <andrzej.wiacek@protonmail.com>
2020-02-01 14:04:27 +0100

    Update item API.

--- src/item.py
+++ src/item.py
@@ -4,20 +4,12 @@ from typing import Any, Dict, List, Tuple, Optional
 from flask_jwt import jwt_required  # type: ignore
 from flask_restful import Resource, reqparse  # type: ignore
 
-items: List[Dict[str, Any]] = []
-
 
 class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument(
         "price", type=float, required=True, help="This field cannot be left blank.",
     )
-
-    @jwt_required()
-    def get(self, name: str) -> Tuple[Dict[str, Any], int]:
-        if item := self.find_item_by_name(name):
-            return item, 200  # type: ignore
-        return {"message": "Item not found."}, 404
 
     @classmethod
     def find_item_by_name(cls, name: str) -> Optional[Dict[str, Any]]:
@@ -27,8 +19,36 @@ class Item(Resource):
         query = "SELECT * FROM ITEMS WHERE name=?"
         result = cursor.execute(query, (name,))
         if row := result.fetchone():
-            return {"item": {"name": row[0], "price": row[1]}}
+            return {"name": row[0], "price": row[1]}
         return None
+
+    @classmethod
+    def insert_item(cls, item: dict) -> None:
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item["name"], item["price"]))
+
+        connection.commit()
+        connection.close()
+
+    @classmethod
+    def update_item(cls, item: dict) -> None:
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item["price"], item["name"]))
+
+        connection.commit()
+        connection.close()
+
+    @jwt_required()
+    def get(self, name: str) -> Tuple[Dict[str, Any], int]:
+        if item := self.find_item_by_name(name):
+            return item, 200  # type: ignore
+        return {"message": "Item not found."}, 404
 
     def post(self, name: str) -> Tuple[Dict[str, Any], int]:
         if self.find_item_by_name(name):
@@ -40,31 +60,43 @@ class Item(Resource):
         data = Item.parser.parse_args()
         item = {"name": name, "price": data["price"]}
 
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
-
-        query = "INSERT INTO items VALUES name=(?, ?)"
-        cursor.execute(query, (name, item["price"]))
-
-        connection.commit()
-        connection.close()
+        try:
+            self.insert_item(item)
+        except sqlite3.OperationalError as error:
+            return {"message": f"Couldnt create item: {error}"}, 500
 
         return item, 201
 
-    def delete(self, name: str) -> Dict[str, str]:
-        global items
-        items = list(filter(lambda x: x["name"] != name, items))
-        return {"message": "Item deleted"}
+    def delete(self, name: str) -> Tuple[Dict[str, str], int]:
+        if self.find_item_by_name(name):
+            connection = sqlite3.connect("data.db")
+            cursor = connection.cursor()
 
-    def put(self, name: str) -> Dict[str, Any]:
+            query = "DELETE FROM items WHERE name=?"
+            cursor.execute(query, (name,))
+
+            connection.commit()
+            connection.close()
+            return {"message": "Item deleted."}, 200
+        return {"message": "Item not found."}, 404
+
+    def put(self, name: str) -> Tuple[Dict[str, Any], int]:
         data = Item.parser.parse_args()
-        item = next(filter(lambda x: x["name"] == name, items), None)
+        item = self.find_item_by_name(name)
         if item is None:
             item = {"name": name, "price": data["price"]}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
+            try:
+                self.insert_item(item)
+            except sqlite3.OperationalError:
+                return {"message": "An error has occurred."}, 500
+            return item, 201
+
+        updated_item = {"name": name, "price": data["price"]}
+        try:
+            self.update_item(updated_item)
+        except sqlite3.OperationalError:
+            return {"message": "An error has occurred."}, 500
+        return updated_item, 200
 
 
 class ItemList(Resource):
