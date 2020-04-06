97463635f6f61bd2fab3a6c237be178868acd745
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2020-04-06 23:17:00 +0200

    Use base64 to store password hashes

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-import bcrypt
-
+import base64
 from typing import Optional
+
+import bcrypt
 
 from db import db
 
@@ -12,11 +13,13 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80))
-    hashed_password = db.Column(db.Bytea(60))
+    hashed_password_b64 = db.Column(db.LargeBinary(80))
 
     def __init__(self, email: str, password: str) -> None:
         self.email = email
-        self.hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        self.hashed_password_b64 = base64.b64encode(
+            bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        )
 
     def save_to_db(self) -> None:
         db.session.add(self)
--- security.py
+++ security.py
@@ -1,12 +1,16 @@
-import bcrypt
+import base64
 from typing import Optional, Dict, Any
+
+import bcrypt
 
 from models.user import UserModel
 
 
 def authenticate(email: str, password: str) -> Optional[UserModel]:
     user = UserModel.find_by_email(email)
-    if user and bcrypt.checkpw(password.encode(), user.hashed_password):
+    if user and bcrypt.checkpw(
+        password.encode(), base64.b64decode(user.hashed_password_b64)
+    ):
         return user
     return None
 
